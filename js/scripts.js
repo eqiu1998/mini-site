d6a654c89c9121b4f4922dafb5f7257b923447e3
Eric Qiu <eqiu1998@hotmail.com>
2019-02-18 16:09:23 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,7 @@ window.onload = function() {
     'Hai 👋',
     'I\'m Eric, a student studying Software Engineering at the University of Waterloo',
     'I like to build tech and take photos in my free time',
-    'I\'m currently studying in school and getting ready to work in Seattle for Summer Internship.<br> You can contact me at <a href="mailto:hello@ericqiu.io">hello@ericqiu.io</a>',
+    'I\'m currently studying in school and getting ready for my next internship this summer!<br> You can contact me at <a href="mailto:hello@ericqiu.io">hello@ericqiu.io</a>',
     '<a target="_blank" href="https://twitter.com/itsericqiu">https://twitter.com/itsericqiu</a><br><a target="_blank" href="https://instagram.com/itsericqiu">https://instagram.com/itsericqiu</a><br><a target="_blank" href="https://linkedin.com/in/eric-qiu">https://linkedin.com/in/eric-qiu</a>',
     getCurrentTime(),
     'Thanks for visiting!',
